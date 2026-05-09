56278fc3fd510ffe293dfb2b6cf231718353756f
Joseph Meyrick <joseph@fxdigital.uk>
2026-05-09 22:09:26 +0100

    use https

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import fs from "node:fs";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: "localhost",
     port: 5000,
+    https: {
+      key: fs.readFileSync("certs/handysign.local-key.pem"),
+      cert: fs.readFileSync("certs/handysign.local-cert.pem"),
+    },
   },
 });
